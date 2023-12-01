b6c9a1ec7e4620783ea87a26f4749f61502afc90
kien0612 <vutrungkien612203@gmail.com>
2023-12-02 00:19:24 +0700

    kien

--- app/views/Client/binhluat/frombinhluat.php
+++ app/views/Client/binhluat/frombinhluat.php
@@ -2,11 +2,9 @@
 session_start();
 include "../../../models/pdo.php";
 include "../../../models/binhluan.php";
-$id_sp=$_REQUEST['id_sp'];
-$list_binh_lua =loadAll_binh_lua($id_sp);  
-
+$id_sp = $_REQUEST['id_sp'];
+$list_binh_lua = loadAll_binh_lua($id_sp);  
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,57 +15,47 @@ $list_binh_lua =loadAll_binh_lua($id_sp);
     <link rel="stylesheet" href="../css/css.css">
 </head>
 <style>
-
 </style>
 <body>
 <div class="mb">
     <!-- list bl -->
+    <div class="box_content2 product_portfolio">
+        <table style="width: 100%;margin-bottom: 10px;">
+            <?php foreach($list_binh_lua as $bl) { ?>
+                <tr>
+                    <th>Khách hàng : <?= $bl['ten_tk']?></th>
+                </tr>
+                <tr style="border-bottom: 1px solid #595959;">
+                    <td><?= $bl['noi_dung_bl'] ?></td>
+                    <td><?= $bl['ngay_bl'] ?></td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
 
-        <div class="box_content2 product_portfolio">
-            <table style="width: 100%;margin-bottom: 10px;">
-                <?php  $list_binh_luan = loadAll_binh_luan();  ?>
-                <?php
-                      foreach($list_binh_lua as $bl){
-                          extract($bl);
-                          ?>
-                          <tr>
-                            <th>Khách hàng : <?= $ten_tk?></th>
-                          </tr>
-                          <tr style="border-bottom: 1px solid #595959;">
-                            <td><?= $noi_dung_bl ?></td>
-                            <td ><?= $ngay_bl ?></td>
-                          </tr>
-                         
-                        <?php
-                      }
-                      ?>
-            </table>
-        </div>
-        
-        <div class="box_search">
-            <!-- thêm binh luât -->
+    <div class="box_search">
+        <?php if(isset($_SESSION['user'])) { ?>
+            <!-- Hiển thị form bình luận khi đã đăng nhập -->
             <form action="<?=$_SERVER['PHP_SELF'];?>" method="POST">
                 <input type="hidden" name="id_sp" value="<?php echo $id_sp?>">
                 <input type="text" style="width: 85%;" name="noi_dung_bl" id="noi_dung_bl" placeholder="Từ khóa tìm kiếm" name="noidung">
                 <input type="submit" value="Gửi bình luận" name="guibinhluan">
             </form>
-        </div>
-
-
-        <?php
-        if (isset($_POST['guibinhluan']) && ($_POST['guibinhluan'])) {
-                $id_sp=$_POST['id_sp'];
-                $noi_dung_bl=$_POST['noi_dung_bl'];
-                $id_tk=$_SESSION['user']['id_tk'];
-                $currentDateTime = new DateTime();
-                $currentDateTimeString = $currentDateTime->format('Y-m-d H:i:s');
-                $ngay_bl= $currentDateTimeString;
-                insert_binhluan($noi_dung_bl,$id_tk,$id_sp,$ngay_bl);
-                header("location: ".$_SERVER['HTTP_REFERER']);
-            }
-            ?>
-      
+        <?php } ?>
     </div>
+
+    <?php
+    if (isset($_POST['guibinhluan']) && isset($_SESSION['user'])) {
+        $id_sp = $_POST['id_sp'];
+        $noi_dung_bl = $_POST['noi_dung_bl'];
+        $id_tk = $_SESSION['user']['id_tk'];
+        $currentDateTime = new DateTime();
+        $currentDateTimeString = $currentDateTime->format('Y-m-d H:i:s');
+        $ngay_bl = $currentDateTimeString;
+        insert_binhluan($noi_dung_bl, $id_tk, $id_sp, $ngay_bl);
+        header("location: ".$_SERVER['HTTP_REFERER']);
+    }
+    ?>
+</div>
 </body>
 </html>
-
