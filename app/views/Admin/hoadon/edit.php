c787bc2c333f7453864635bccf8945bf39975bf6
dovanduc03 <ducdovan2032gmail.com>
2023-12-06 22:11:56 +0700

    duc

--- app/views/Admin/hoadon/edit.php
+++ app/views/Admin/hoadon/edit.php
@@ -11,9 +11,32 @@ if (is_array($hoa_don)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
+<center>
 <body>
-    <center>
+    <h1>Thông Tin Nguời Nhận</h1>
+ 
+  
+              
+                Tên Người Nhận: <label for=""><?php echo $hoten?></label> <br>
+                SỐ Điện Thoại:<label for=""><?php echo $sdt?></label><br>
+                Email: <label for=""><?php echo $email?></label> <br>
+                Địa Chỉ: <label for=""><?php echo $diachi?></label> <br>
+                Ngày Đạt Hàng:  <label for=""><?php echo $ngaydathang?></label> <br>
+                Mô Tả: <label for=""><?php echo $mota?></label> <br>
+                Tổng Tiền: <label for=""><?php echo $id_tk?></label> <br>
+
+               <h2>Thông Tin Đơn Hàng</h2>
+               Tên sản phẩn: <label for=""><?php echo $ten_sp?></label> <br>
+               Tên Người Nhận: <label for=""><?php echo $hoten?></label> <br>
+               Tên Người Nhận: <label for=""><?php echo $hoten?></label> <br>
+               Tên Người Nhận: <label for=""><?php echo $hoten?></label> <br>
+               Tên Người Nhận: <label for=""><?php echo $hoten?></label> <br>
+               Tên Người Nhận: <label for=""><?php echo $hoten?></label> <br>
+
+        
+       
+
+   
         <form action="?act=update" method="post">
             <input type="hidden" name="id_hd" id="" value="<?= $id_hd ?>">
             <select name="trangthai">
