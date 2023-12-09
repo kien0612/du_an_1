2dcd3d0cba2a074d372c524b67c458e2123fb1b1
dovanduc03 <ducdovan2032gmail.com>
2023-12-09 12:11:21 +0700

    duc

--- app/models/hoadon.php
+++ app/models/hoadon.php
@@ -1,13 +1,12 @@
 <?php
 
-
 function loadAll_hoa_don(){
     $sql = "SELECT * FROM `hoadon` JOIN `taikhoan` ON hoadon.id_tk = taikhoan.id_tk JOIN `hoadonchitet` ON hoadonchitet.id_hd = hoadon.id_hd JOIN `sanpham` ON sanpham.id_sp = hoadonchitet.id_sp join trangthaidoihang on hoadon.trangthai=trangthaidoihang.id_thdh ";
     $list_hoa_don = pdo_query($sql);
     return $list_hoa_don;
 }
-function delete_hoa_don_ct($id_hdct){
-    $sql = "DELETE FROM `hoadonchitet` WHERE id_hdct=" .$id_hdct;
+function delete_hoa_don($id_hd){
+    $sql = "DELETE FROM `hoadon` WHERE id_hd=" .$id_hd;
     pdo_execute($sql); 
 }
 function loadAll_lich_su_dh(){
@@ -25,7 +24,27 @@ function list_hoadonadmin(){
     return $list_admin;
 }
 function list_hoadon(){
-    $sql="SELECT * FROM `hoadon` JOIN `taikhoan` ON hoadon.id_tk = taikhoan.id_tk JOIN `hoadonchitet` ON hoadonchitet.id_hd = hoadon.id_hd JOIN `sanpham` ON sanpham.id_sp = hoadonchitet.id_sp join trangthaidoihang on hoadon.trangthai=trangthaidoihang.id_thdh";
+    $sql="SELECT hoadon.id_hd, hoadon.tongtien, taikhoan.full_name, hoadon.sdt, hoadon.email, hoadon.ngaydathang ,hoadon.diachi, hoadon.pttt, trangthaidoihang.ten_dh, hoadon.mota, hoadon.id_tk, 
+    SUM(hoadonchitet.soluong) AS tong_soluong
+FROM hoadon
+JOIN taikhoan ON hoadon.id_tk = taikhoan.id_tk 
+JOIN hoadonchitet ON hoadonchitet.id_hd = hoadon.id_hd 
+JOIN sanpham ON sanpham.id_sp = hoadonchitet.id_sp 
+JOIN trangthaidoihang ON hoadon.trangthai = trangthaidoihang.id_thdh
+GROUP BY hoadon.id_hd, hoadon.tongtien, hoadon.hoten, hoadon.sdt, hoadon.email, hoadon.ngaydathang, hoadon.diachi, hoadon.pttt, hoadon.trangthai, hoadon.mota, hoadon.id_tk;";
     $list_admin_hd=pdo_query($sql);
     return $list_admin_hd;
+}
+
+function list_hdct($id_hd){
+    $sql = "SELECT * FROM hoadonchitet
+    WHERE hoadon.id_hd =" .$id_hd;
+    pdo_execute($sql);
+}
+function list_hdct_view(){
+    $sql = "SELECT * FROM hoadon 
+    JOIN hoadonchitet on hoadon.id_hd = hoadonchitet.id_hd
+    JOIN sanpham ON sanpham.id_sp = hoadonchitet.id_sp";
+    $list_hdct_view = pdo_query($sql);
+    return $list_hdct_view;
 }
